787a8a3f6cb1fdaafd8544eb4d6a891d459732fd
yash6759 <58842187+yash6759@users.noreply.github.com>
2020-02-15 22:14:47 +0530

    Update modmail.py

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -98,8 +98,8 @@ class Modmail(commands.Cog):
         embed.add_field(
             name="Thanks for using our bot!",
             value="If you like what you see, consider giving the "
-            "[repo a star](https://github.com/kyb3r/modmail) :star: and if you are "
-            "feeling extra generous, buy us coffee on [Patreon](https://patreon.com/kyber) :heart:!",
+            "[🧡🧡🧡](https://github.com) :star:",
+            
         )
 
         embed.set_footer(text=f'Type "{self.bot.prefix}help" for a complete list of commands.')
